77d2093307f66e4ee3b0e8a39563505f0085bbb0
r20031646 <ramdani.meriem2001@gmail.com>
2022-11-10 11:26:54 +0100

    first commit

---
+++ src/main/java/Fire.java
@@ -0,0 +1,11 @@
+public class Fire implements Element{
+    @Override
+    public void activation() {
+
+    }
+
+    @Override
+    public void Paint() {
+
+    }
+}
---
+++ src/main/java/Position.java
@@ -0,0 +1,11 @@
+public class Position {
+
+    double row;
+    double col;
+
+    public Position(double row, double col) {
+        this.row =row;
+        this.col= col;
+
+    }
+}
