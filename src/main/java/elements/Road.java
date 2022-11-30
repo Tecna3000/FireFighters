d1678ef808f4f08c5daa4574380c6ddaa7ad9800
r20031646 <ramdani.meriem2001@gmail.com>
2022-11-30 17:47:43 +0100

    add road

--- src/main/java/elements/Road.java
+++ src/main/java/elements/Road.java
@@ -9,36 +9,33 @@ import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Mountains extends Elements{
-    List<Position> mountainsList = new ArrayList<>();
-    public Mountains(Model model) {
+public class Road extends Elements{
+    public Road(Model model) {
         super(model);
     }
-    Image mountainImage;
+    List<Position> roadPositions = new ArrayList<>();
+    Image roadImage;
 
     {
         try {
-            mountainImage = new Image( new File("../firefighterstarter/src/main/java/view/images/mountain.png").toURI().toURL().toString());
+            roadImage = new Image( new File("../firefighterstarter/src/main/java/view/images/road.png").toURI().toURL().toString());
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
     }
+
     @Override
     public void initialisation(int number) {
         for (int index = 0; index < number; index++)
-            mountainsList.add(model.randomPosition());
+            roadPositions.add(model.randomPosition());
+
     }
 
     @Override
     public void activation() {
-        for(Position mountain : mountainsList)
-            model.painter.paintElement(mountainImage,mountain.row(),mountain.col());
-    }
-    public List<Position> getMountainsPositions() {
-        return mountainsList;
-    }
+        for(Position road : roadPositions) model.painter.paintElement(roadImage,road.row(),road.col());
 
-
+    }
 
     @Override
     Position move(Position position) {
